a89c20d0eedf4c6eff151067ccbf2a9973214195
Mandy <Mandy>
2019-07-15 03:51:13 -0700

    最新print

--- DiDi/ViewController.swift
+++ DiDi/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("修改哟")
+        print("是你的")
     }
 
 
